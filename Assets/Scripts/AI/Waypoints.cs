a474681aea927c745c89f54f6f6224dd5a33b0f4
Alvarden <alvaroracchumi@hotmail.com>
2022-10-30 18:54:10 -0500

    chore(Nivel 2): mecanicas basicas implementadas al nivel 2

--- Assets/Scripts/AI/Waypoints.cs
+++ Assets/Scripts/AI/Waypoints.cs
@@ -1,32 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Nocturne.GeneralTools;
-using Pathfinding;
+using System.Collections;
+using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
     public Transform[] positions;
 
-    int selectedIndex;
+    private int selectedIndex;
 
     [SerializeField]
-    bool randomPath;
+    private bool randomPath;
 
     [SerializeField]
-    EnemyAIStandard targetAI;
+    private EnemyAIStandard targetAI;
 
     [SerializeField]
-    float timeInterval = 4f;
+    private float timeInterval = 4f;
 
-    void Start()
+    private void Start()
     {
-        if(targetAI == null)
+        if (targetAI == null)
         {
             Debug.LogError("Asigna un AI que utilizara estoy waypoints");
         }
 
-        if(positions.Length < 2)
+        if (positions.Length < 2)
         {
             Debug.LogError("Debe haber más de 1 posicion establecida para el waypoint");
         }
@@ -34,11 +32,21 @@ public class Waypoints : MonoBehaviour
         selectedIndex = Random.Range(0, positions.Length - 1);
         targetAI.SetDestination(positions[selectedIndex]);
     }
-    
+
     public void ChangeWaypoint()
     {
-        if(targetAI.currentStatus == EnemyStatus.Patrolling)
+        if (targetAI.currentStatus == EnemyStatus.Patrolling)
+        {
             StartCoroutine(ChangePosition());
+        }
+    }
+
+    public void ChangeWaypoint(float time)
+    {
+        if (targetAI.currentStatus == EnemyStatus.Patrolling)
+        {
+            StartCoroutine(ChangePosition(time));
+        }
     }
 
     public void QuitPatrol()
@@ -46,9 +54,10 @@ public class Waypoints : MonoBehaviour
         StopCoroutine(ChangePosition());
     }
 
-    IEnumerator ChangePosition()
+    private IEnumerator ChangePosition()
     {
         yield return Helpers.GetWait(timeInterval);
+
         if (targetAI.currentStatus == EnemyStatus.Patrolling)
         {
             if (randomPath)
@@ -67,7 +76,29 @@ public class Waypoints : MonoBehaviour
                 targetAI.SetDestination(positions[selectedIndex]);
             }
         }
-        
+    }
 
+    private IEnumerator ChangePosition(float time)
+    {
+        yield return Helpers.GetWait(time);
+
+        if (targetAI.currentStatus == EnemyStatus.Patrolling)
+        {
+            if (randomPath)
+            {
+                int newIndex = Random.Range(0, positions.Length - 1);
+                selectedIndex = newIndex;
+                targetAI.SetDestination(positions[selectedIndex]);
+            }
+            else
+            {
+                selectedIndex++;
+                if (selectedIndex >= positions.Length)
+                {
+                    selectedIndex = 0;
+                }
+                targetAI.SetDestination(positions[selectedIndex]);
+            }
+        }
     }
 }
